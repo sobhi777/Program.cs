9d87d6e97acd44361b6d987fefe73785482aacd4
Subhi Haj ahmad <subhiahmad2016@gmail.com>
2025-10-29 08:50:34 -0400

    Add files via upload
    Signed-off-by: Subhi Haj ahmad <subhiahmad2016@gmail.com>

---
+++ InsuranceEntities.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace InsuranceMVC.Models
+{
+    public class InsuranceEntities : DbContext
+    {
+        public InsuranceEntities(DbContextOptions<InsuranceEntities> options) : base(options) { }
+
+        public DbSet<Insuree> Insurees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Insuree>().ToTable("Insurees");
+        }
+    }
+}
---
+++ Insuree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceMVC.Models
+{
+    public class Insuree
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(1, 120)]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Car Year")]
+        public int CarYear { get; set; }
+
+        [Required]
+        [Display(Name = "Car Make")]
+        public string CarMake { get; set; }
+
+        [Required]
+        [Display(Name = "Car Model")]
+        public string CarModel { get; set; }
+
+        [Required]
+        [Display(Name = "DUI")]
+        public bool HasDUI { get; set; }
+
+        [Required]
+        [Display(Name = "Speeding Tickets")]
+        [Range(0, 100)]
+        public int SpeedingTickets { get; set; }
+
+        [Required]
+        [Display(Name = "Full Coverage")]
+        public bool FullCoverage { get; set; }
+
+        [Display(Name = "Quote")]
+        [DataType(DataType.Currency)]
+        public decimal Quote { get; set; }
+    }
+}
